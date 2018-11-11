16d6a1f1bc9d497724906ae1afacfd2388849147
Siddhartha Maji <siddhartha.ee.13@gmail.com>
2018-11-12 02:32:22 +0530

    Initial commit

---
+++ app/src/main/java/in/creativelizard/jetpackdemo/ui/main/MainViewModel.kt
@@ -0,0 +1,10 @@
+package `in`.creativelizard.jetpackdemo.ui.main
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+
+     var name: MutableLiveData<String> = MutableLiveData()
+}
